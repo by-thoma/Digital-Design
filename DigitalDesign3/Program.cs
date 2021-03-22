dd153c0b0f7c123fca37a720166e8cfdcdcab8f9
Tamara Tsogoeva <63509284+tomaXss@users.noreply.github.com>
2021-03-22 20:20:38 +0300

    first commit

---
+++ DigitalDesign3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Library;
+using System.Diagnostics;
+
+
+namespace Тестовое
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            string filename = @"C:/Users/toma0/OneDrive/Рабочий стол/Война и мир.fb2";
+            string sr = File.ReadAllText(filename, Encoding.UTF8);
+            if (sr != null)//проверка на пустой файл
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Type type = typeof(Methods);//получаем ссылку на объект, содержащий информацию о Class1
+                ConstructorInfo ctor = type.GetConstructor(new Type[] { });//поиск конструктора
+                var result = ctor.Invoke(new object[] { });//вызов конструктора
+                var Book = (Dictionary<string, int>)type.GetMethod("ToDict", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(result, new object[] { sr });//вызов приватного метода ToDict
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;//время, затраченное на выполнение части программы
+                string elapsedTime = String.Format("{0:00}", ts.Seconds);//формат
+                Console.WriteLine("Private method: " + elapsedTime + "sec");
+
+
+                stopWatch.Restart();
+                Methods.ToDictForeach(sr);
+                stopWatch.Stop();
+                ts = stopWatch.Elapsed;
+                elapsedTime = String.Format("{0:00}", ts.Seconds);
+                Console.WriteLine("Public method: " + elapsedTime + "sec");
+
+                using StreamWriter wordsintext = new StreamWriter(@"C:/Users/toma0/OneDrive/Рабочий стол/WordsInText.txt");
+                {
+                    foreach (var i in Book.OrderByDescending(x => x.Value))
+                    {
+                        wordsintext.WriteLine($"{i.Key} - {i.Value}");
+                    }//Вывод словаря в текстовый файл
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пустой документ");
+            }
+        }
+    }
+
+}
---
+++ Library/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Library
+{
+    public class Methods
+    {
+
+        private Dictionary<string, int> ToDict(string st)
+        {
+            Dictionary<string, int> book = new Dictionary<string, int>();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '<', '>', '\n', ')', '(', '!', '?', ';', '"', ']', '[', '/', '-' };//разделители
+            string[] words = st.Split(delimiterChars).Select(word => word.ToLower()).ToArray();
+            //Split - разбивает строку на подстроки            
+            //ToLower - возвращает строку в нижнем регистре
+            string[] uniquewords = words.Distinct().ToArray();
+            //Distinct - возвращает уникальные строки
+            foreach (string i in uniquewords)
+            {
+                int count = 0;
+                count = words.Where(word => word.Equals(i)).Count();
+                //подсчет одинаковых строк
+                book.Add(i, count);
+            }
+
+            return book;
+        }
+
+        public static Dictionary<string, int> ToDictForeach(string st)
+        {
+            Dictionary<string, int> book = new Dictionary<string, int>();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '<', '>', '\n', ')', '(', '!', '?', ';', '"', ']', '[', '/', '-' };
+            string[] words = st.Split(delimiterChars).Select(word => word.ToLower()).ToArray();
+
+            string[] uniquewords = null;
+
+            uniquewords = words.Distinct().ToArray();
+
+            Parallel.ForEach(uniquewords, word =>
+            {
+                int count = 0;
+                count = words.Where(ex => ex.Equals(word)).Count();
+                book.Add(word, count);
+            });//параллельная работа с элементами uniquewords
+            return book;
+
+        }
+
+    }
+}
+
